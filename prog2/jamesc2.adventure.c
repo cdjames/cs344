521996968481814de13ddf69a123db0b721e551d
Collin James <mail@collinjam.es>
2016-10-23 20:10:01 -0400

    createFiles is pretty much feature-complete

--- prog2/jamesc2.adventure.c
+++ prog2/jamesc2.adventure.c
@@ -6,21 +6,82 @@
 #include <sys/types.h> // for struct stat
 #include <sys/stat.h> // for stat()
 #include <string.h>  // for memset
+#include <time.h>
+
+int getRandom(int min, int max) {
+	return (rand() % (max+1-min) + min);
+}
 
 int createFiles(char * filepath, char * dirName, char ** rooms, int len){
 	int i;
-	for (i = 0; i < 7; i++)
+	int file_descriptor;
+	int loops = 7;
+	char rmName[] = "ROOM NAME: ";
+	char cnxn[] = "CONNECTION ";
+	char rmType[] = "ROOM TYPE: ";
+	char typeTxt[10];
+	char prntName[30];
+	char prntCnxn[40];
+	char prntType[30];
+
+	for (i = 0; i < loops; i++)
 	{
+		/* open/create files */
 		memset(filepath, '\0', len);
 		sprintf(filepath, "%s/%s.room", dirName, rooms[i]);
-		int file_descriptor = open(filepath, O_RDWR | O_CREAT | O_APPEND, 0644);
-		write(file_descriptor, rooms[i], strlen(rooms[i]) * sizeof(char));
+		file_descriptor = open(filepath, O_RDWR | O_CREAT | O_APPEND, 0644);
+		if (file_descriptor == -1)
+		{
+			return 1;
+		}
+
+		/* prepare strings */
+		memset(typeTxt, '\0', 10);
+		if(i == 0)
+			sprintf(typeTxt,"START_ROOM");
+		else if(i < loops-1)
+			sprintf(typeTxt,"MID_ROOM");
+		else
+			sprintf(typeTxt,"END_ROOM");
+		memset(prntName, '\0', 30);
+		memset(prntCnxn, '\0', 40);
+		memset(prntType, '\0', 30);
+		sprintf(prntName, "%s%s\n", rmName, rooms[i]);
+		sprintf(prntType, "%s%s\n", rmType, typeTxt);
+		// int rmNum = i+1;
+		// if(rmNum >= loops)
+		// 	rmNum = 0;
+		// sprintf(prntCnxn, "%s%d: %s\n", cnxn, 1, rooms[rmNum]);
+
+		/* write to file */
+		write(file_descriptor, prntName, strlen(prntName) * sizeof(char));
+		// figure connections
+		int x;
+		int start = getRandom(0,6);
+		while(start == i)
+			start = getRandom(0,6);
+		int numLoops = getRandom(3,6);
+		for (x = 0; x < numLoops; x++)
+		{
+			if(start == i)
+				start++;
+				if(start >= loops)
+					start = 0;
+			sprintf(prntCnxn, "%s%d: %s\n", cnxn, 1, rooms[start++]);
+			write(file_descriptor, prntCnxn, strlen(prntCnxn) * sizeof(char));
+		}
+		
+		write(file_descriptor, prntType, strlen(prntType) * sizeof(char));
 		close(file_descriptor);
 	}
+	return 0;
 }
+
+
 
 int main(int argc, char const *argv[])
 {
+	srand(time(NULL));
 	/* create directory name */
 	char dirName[30];
 	int pid = getpid();
@@ -50,9 +111,10 @@ int main(int argc, char const *argv[])
 	/* create files */
 	int file_descriptor;
 	char filepath[50];
-	createFiles(filepath, dirName, rooms, 50);
-
-	
+	int file_status = createFiles(filepath, dirName, rooms, 50);
+	/* exit if file(s) couldn't be created */
+	if(file_status == 1)
+		exit(1);
 
 	return 0;
 }
