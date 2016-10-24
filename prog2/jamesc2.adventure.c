050fe198e917bc25b5e174738592f3d32eca17e6
Collin James <mail@collinjam.es>
2016-10-24 18:07:16 -0400

    almost good with random room generation. need to fix room linked to self

--- prog2/jamesc2.adventure.c
+++ prog2/jamesc2.adventure.c
@@ -1,34 +1,51 @@
 // compile with gcc –o jamesc2.adventure jamesc2.adventure.c -lpthread
-#include <fcntl.h>
-#include <stdio.h>	// printing, etc
-#include <stdlib.h>	// mkdir, etc.
-#include <unistd.h> // for process id
-#include <sys/types.h> // for struct stat
-#include <sys/stat.h> // for stat()
-#include <string.h>  // for memset
+#include <fcntl.h>		// for file manipulation
+#include <stdio.h>		// printing, etc
+#include <stdlib.h>		// mkdir, etc.
+#include <unistd.h> 	// for process id
+#include <sys/types.h> 	// for struct stat
+#include <sys/stat.h> 	// for stat()
+#include <string.h>  	// for memset
 #include <time.h>
+
+const int NUM_ROOMS = 7;
 
 int getRandom(int min, int max) {
 	return (rand() % (max+1-min) + min);
 }
 
-int createFiles(char * filepath, char * dirName, char ** rooms, int len){
-	int i;
-	int file_descriptor;
-	int loops = 7;
-	char rmName[] = "ROOM NAME: ";
-	char cnxn[] = "CONNECTION ";
-	char rmType[] = "ROOM TYPE: ";
-	char typeTxt[10];
-	char prntName[30];
-	char prntCnxn[40];
-	char prntType[30];
+void clearString(char * theString, int size) {
+	memset(theString, '\0', size);
+}
 
-	for (i = 0; i < loops; i++)
+int adjustIdx(int start, int maxIdx){
+	if(start > maxIdx)
+		start = start - (maxIdx+1);
+	return start;
+}
+
+int createFiles(char * filepath, char * dirName, char ** rooms, int len, int startPt, int endPt){
+	int file_descriptor,
+		maxIdx = 9,
+		i,
+		curIdx;
+	char rmName[] = "ROOM NAME: ",
+	 	cnxn[] = "CONNECTION ",
+	 	rmType[] = "ROOM TYPE: ",
+	 	typeTxt[10],
+	 	prntName[30],
+	 	prntCnxn[40],
+	 	prntType[30];
+
+	
+	for (i = startPt; i < endPt; i++)
 	{
+		curIdx = adjustIdx(i, maxIdx);
+
 		/* open/create files */
-		memset(filepath, '\0', len);
-		sprintf(filepath, "%s/%s.room", dirName, rooms[i]);
+		clearString(filepath, len);
+		// memset(filepath, '\0', len);
+		sprintf(filepath, "%s/%s.room", dirName, rooms[curIdx]);
 		file_descriptor = open(filepath, O_RDWR | O_CREAT | O_APPEND, 0644);
 		if (file_descriptor == -1)
 		{
@@ -36,47 +53,51 @@ int createFiles(char * filepath, char * dirName, char ** rooms, int len){
 		}
 
 		/* prepare strings */
-		memset(typeTxt, '\0', 10);
-		if(i == 0)
+		clearString(typeTxt, 10);
+		// memset(typeTxt, '\0', 10);
+		if(i == startPt)
 			sprintf(typeTxt,"START_ROOM");
-		else if(i < loops-1)
+		else if(i < endPt-1)
 			sprintf(typeTxt,"MID_ROOM");
 		else
 			sprintf(typeTxt,"END_ROOM");
-		memset(prntName, '\0', 30);
-		memset(prntCnxn, '\0', 40);
-		memset(prntType, '\0', 30);
-		sprintf(prntName, "%s%s\n", rmName, rooms[i]);
+		// memset(prntName, '\0', 30);
+		clearString(prntName, 30);
+		clearString(prntCnxn, 40);
+		clearString(prntType, 30);
+		// memset(prntCnxn, '\0', 40);
+		// memset(prntType, '\0', 30);
+		sprintf(prntName, "%s%s\n", rmName, rooms[curIdx]);
 		sprintf(prntType, "%s%s\n", rmType, typeTxt);
-		// int rmNum = i+1;
-		// if(rmNum >= loops)
-		// 	rmNum = 0;
-		// sprintf(prntCnxn, "%s%d: %s\n", cnxn, 1, rooms[rmNum]);
 
 		/* write to file */
 		write(file_descriptor, prntName, strlen(prntName) * sizeof(char));
 		// figure connections
-		int x;
-		int start = getRandom(0,6);
+		int x,
+			getIdx = getRandom(startPt,endPt);
 		while(start == i)
-			start = getRandom(0,6);
+			getIdx = getRandom(startPt,endPt);
+
 		int numLoops = getRandom(3,6);
 		for (x = 0; x < numLoops; x++)
 		{
-			if(start == i)
-				start++;
-				if(start >= loops)
-					start = 0;
-			sprintf(prntCnxn, "%s%d: %s\n", cnxn, 1, rooms[start++]);
+			if(getIdx == i)			// don't want a room linked to itself!
+				getIdx++;
+			if(getIdx >= endPt)		// make sure we don't include rooms that don't matter! (endPt-1 adjusted is last index in group of rooms)
+				getIdx = startPt;
+			clearString(prntCnxn, 40);
+			sprintf(prntCnxn, "%s%d: %s\n", cnxn, 1, rooms[adjustIdx(getIdx,maxIdx)]);
 			write(file_descriptor, prntCnxn, strlen(prntCnxn) * sizeof(char));
+			getIdx++;
 		}
-		
+		/* finish writing */
 		write(file_descriptor, prntType, strlen(prntType) * sizeof(char));
 		close(file_descriptor);
+
 	}
+	
 	return 0;
 }
-
 
 
 int main(int argc, char const *argv[])
@@ -109,12 +130,16 @@ int main(int argc, char const *argv[])
 	rooms[9] = "Bedroom";
 
 	/* create files */
-	int file_descriptor;
 	char filepath[50];
-	int file_status = createFiles(filepath, dirName, rooms, 50);
+	int file_descriptor,
+		startPt = getRandom(0,9),
+		file_status = createFiles(filepath, dirName, rooms, 50, startPt, startPt+NUM_ROOMS);
 	/* exit if file(s) couldn't be created */
 	if(file_status == 1)
 		exit(1);
+
+	/* read start file and present to user */
+
 
 	return 0;
 }
