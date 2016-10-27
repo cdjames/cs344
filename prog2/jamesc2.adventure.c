8e0e828e4bf9ecf580bae0915c1b55802df9c1b5
Collin James <mail@collinjam.es>
2016-10-27 18:53:28 -0400

    done, test and submit

--- prog2/jamesc2.adventure.c
+++ prog2/jamesc2.adventure.c
@@ -21,7 +21,93 @@ const int RM_NM_RD = 11;	// point in string to start reading (ROOM TYPE: & ROOM 
 const int CXN_NM_RD = 14;	// point in string to start reading (CONNECTION #: )
 const int MAXIDX = 9;		// max index in size 10 array
 const char * DIR = "jamesc2.adventure"; // beginning of directory name
-const char * DELIM = ";";	// delimeter used in various string operations
+const char * DELIM = ";";	// delimiter used in various string operations
+
+/* function declarations; functions and descriptions follow main() */
+int getRandom(int min, int max);
+void clearString(char * theString, int size);
+int adjustIdx(int idx, int maxIdx);
+int createFiles(char * dirName, char ** rooms, int len, int startPt, int endPt);
+int validateInput(char * input, char * rooms);
+void getInput(char **retString, char * roomNmOnly, char * allRooms, char * possRooms);
+void printAndGetInput(FILE * file, char **nextRoom);
+char figureRoomType(FILE * file, signed int seekFrom, unsigned int size);
+int doRoomActions(char * dir, char * room, char **nextRoom, char **path);
+void doEnding(char * path, int roomCount);
+
+
+/*********************************************************************
+** Description: 
+** The main process for the game. Create variables, directory, files,
+** then read start room, present, and get user input. Loop until end room
+** has been found. Print a message, free memory, and exit 
+*********************************************************************/
+int main()
+{
+	/* seed the randomizer */
+	srand(time(NULL));
+	
+	/** variables **/
+	int pid = getpid(), 			// process id
+		result = -1,				// if 1, game over
+		roomCount = 0,				// # rooms visited
+		startPt = getRandom(0,9), 	// start is a random room
+		file_status;				// hold status of opened file (1 is error)
+
+	char dirName[30];				// hold a directory name
+	clearString(dirName, 30);
+	sprintf(dirName, "%s.%d", DIR, pid); 	// fill dirName: jamesc.adventure.####
+	char * input;	// hold user input; init with malloc, needs to be freed
+	char * path;	// hold string of visited rooms; init with malloc, needs to be freed
+	path = malloc( sizeof(char) * 200 ); // allocate 200 bytes for path
+	
+	/* create room/file name array */
+	char * rooms[10];
+	rooms[0] = "Foyer";
+	rooms[1] = "Drawing";
+	rooms[2] = "Office";
+	rooms[3] = "Living";
+	rooms[4] = "Attic";
+	rooms[5] = "Pantry";
+	rooms[6] = "Closet";
+	rooms[7] = "Basement";
+	rooms[8] = "Dining";
+	rooms[9] = "Bedroom";
+
+	/* create directory, checking for existence first */
+	struct stat checkfor;
+	if (stat(dirName, &checkfor) == -1) {
+		/* create directory with rwx ___ ___ (don't let others cheat!) */
+	    mkdir(dirName, 0700);
+	}
+
+	/* open/create files */
+	file_status = createFiles(dirName, rooms, 50, startPt, startPt+NUM_ROOMS);
+	/* exit if file(s) couldn't be created */
+	if(file_status == 1)
+		exit(1);
+
+	/* read start file and present to user */
+	result = doRoomActions(dirName, rooms[startPt], &input, &path);
+
+	/* continue through the remaining rooms */
+	while(result != 1){
+		/* read file selected by user and present */
+		result = doRoomActions(dirName, input, &input, &path);
+		/* increment turn counter */
+		roomCount++;
+	}
+
+	/* if you're out of the loop, that means the last room has been found */
+	doEnding(path, roomCount);
+	
+	/* free variables initialized with malloc */
+	free(input);
+	free(path);
+
+	/* end program with no errors */
+	return 0;
+}
 
 /* functions */
 /*********************************************************************
@@ -184,12 +270,13 @@ void getInput(char **retString, char * roomNmOnly, char * allRooms, char * possR
 }
 
 /*********************************************************************
-** Description: Read a file from beginning to last connection, print
+** Description: Read a file from beginning to last connection, store values
+** in variables, and print via getInput()
 ** file = FILE pointer to hold file status
 ** nextRoom = pointer to a char *, will be sent to getInput() and "returned" with name of next room
 *********************************************************************/
 void printAndGetInput(FILE * file, char **nextRoom){
-	char allRooms[100],
+	char allRooms[100],			// 
 	 	buffer[100],
 	 	roomNameOnly[10],
 	 	roomOnly[11],
@@ -209,23 +296,28 @@ void printAndGetInput(FILE * file, char **nextRoom){
 		if(buffer[0] == 'R'){
 			/* process room name - ROOM (N)AME */
 			if(buffer[5] == 'N'){
-				/* process name by copying info in line AFTER */
+				/* process name by copying info in buffer AFTER location RM_NM_RD */
+				/* only copy max 8 characters */
 				clearString(roomNameOnly, 10);
 				strncpy(roomNameOnly, buffer+RM_NM_RD, 8);
+				/* manually get rid of \n at end of strings */
 				if(roomNameOnly[strlen(roomNameOnly)-1] == '\n'){
-					/* get rid of \n at end of string */
 					roomNameOnly[strlen(roomNameOnly)-1] = '\0';
 				}
 			} // note that ROOM TYPE lines are not processed!
 		} 
 		else {
-			/* process connection*/
+			/* process connection room name, similar to above */
 			clearString(roomOnly, 11);
 			strncpy(roomOnly, buffer+CXN_NM_RD, 9);
-			roomOnly[strlen(roomOnly)-1] = '\0';
+			roomOnly[strlen(roomOnly)-1] = '\0'; // lose \n
+			/* append connection room name to possRooms string */
 			strcat(possRooms, roomOnly);
+			/* append a delimiter */ 
 			strcat(possRooms, DELIM);
+			/* append a comma and space after room name*/
 			strcat(roomOnly, ", ");
+			/* append room name to string of all rooms (Foyer, Attic, )*/
 			strncat(allRooms, roomOnly, strlen(roomOnly));
 		}
 		// i++;
@@ -239,21 +331,40 @@ void printAndGetInput(FILE * file, char **nextRoom){
 	getInput(nextRoom, roomNameOnly, allRooms, possRooms);
 }
 
+/*********************************************************************
+** Description: 
+** Read the last line of a file and return the first letter of 
+** (S)TART_ROOM, (M)ID_ROOM, (E)ND_ROOM, thus determmining room type
+** file = open FILE pointer
+** seekFrom = negative value greater (abs value) than length of last line
+** size = length of string to hold line
+*********************************************************************/
 char figureRoomType(FILE * file, signed int seekFrom, unsigned int size){
-	/* figure process type */
+	/* prepare your buffer*/
 	char buffer[size];
 	clearString(buffer, size);
-
+	/* go back seekFrom places from end of file */
 	fseek(file, seekFrom, SEEK_END);
-	fgets (buffer, size, file);
+	/* get a line */
+	fgets(buffer, size, file);
+	/* if the line doesn't begin with 'R', get next line which should begin w/ 'R' */
 	while(buffer[0] != 'R'){
 		clearString(buffer, size);
-		fgets (buffer, size, file);
+		fgets(buffer, size, file);
 	}
-
+	/* return S, M, or E */
 	return buffer[RM_NM_RD];
 }
 
+/*********************************************************************
+** Description: 
+** Open a file, get room type w/ figureRoomType(), and proceed accordingly,
+** either going to ending actions or getting inputs
+** dir = directory name
+** room = name of room file to open
+** nextRoom = pointer to char *, passed along to printAndGetInput()
+** path = pointer to char *, append visited rooms to this
+*********************************************************************/
 int doRoomActions(char * dir, char * room, char **nextRoom, char **path){
 	char filepath[50];
 	clearString(filepath, 50);
@@ -271,102 +382,43 @@ int doRoomActions(char * dir, char * room, char **nextRoom, char **path){
 
 		/* check for S, M, E (start, mid, end)*/
 		if(letter == 'E'){
+			/* close and return 1 to do game over stuff */
 			fclose(file);
 			return 1;
-			/* do game over stuff */
 		}
 		else {
-
+			/* print rooms and get input */
 			printAndGetInput(file, nextRoom);
-			/* append the next room to the path */
+			/* append the next room plus delimiter to the path for printing at the end of game */
+			/* i.e. "Foyer;Attic;Basement;" */
 			strcat(*path, *nextRoom);
 			strcat(*path, DELIM);
 		}
 	}
+	/* close the file and return 0 to show game should continue */
 	fclose(file);
 	printf("\n");
 	return 0;
 }
 
+/*********************************************************************
+** Description: 
+** Print out a congratulatory message and visited rooms
+** path = list of visited rooms delimited by ";"
+** roomCount = number of rooms visited
+*********************************************************************/
 void doEnding(char * path, int roomCount){
+	/* print message for user */
 	printf("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n");
 	printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", roomCount);
 	/* print visited rooms */
-	/* tokenize path string and print out each token in turn */
+	/* tokenize path string on ';' and print out each token (room) in turn */
 	char * subString;
 	subString = strtok(path,DELIM);
+	/* print out substring and get next one, if none strtok returns NULL */
 	while (subString != NULL)
 	{
 		printf("%s\n", subString);
 		subString = strtok(NULL, DELIM);
 	}
 }	
-
-int main(int argc, char const *argv[])
-{
-	/* seed the randomizer */
-	srand(time(NULL));
-	
-	/** variables **/
-	int pid = getpid(),
-		result = -1,
-		roomCount = 0,
-		file_descriptor,
-		startPt = getRandom(0,9),
-		file_status;
-
-	char dirName[30];
-	clearString(dirName, 30);
-	sprintf(dirName, "%s.%d", DIR, pid); 	// fill dirName: jamesc.adventure.####
-	char * input;	// init with malloc, needs to be freed
-	char * path;	// init with malloc, needs to be freed
-	path = malloc( sizeof(char) * 200 );
-	
-	/* create room/file name array */
-	char * rooms[10];
-	rooms[0] = "Foyer";
-	rooms[1] = "Drawing";
-	rooms[2] = "Office";
-	rooms[3] = "Living";
-	rooms[4] = "Attic";
-	rooms[5] = "Pantry";
-	rooms[6] = "Closet";
-	rooms[7] = "Basement";
-	rooms[8] = "Dining";
-	rooms[9] = "Bedroom";
-
-	/* create directory, checking for existence first */
-	struct stat checkfor;
-	if (stat(dirName, &checkfor) == -1) {
-		/* create directory with rwx ___ ___ (don't let others cheat!) */
-	    mkdir(dirName, 0700);
-	}
-
-	/* open/create files */
-	file_status = createFiles(dirName, rooms, 50, startPt, startPt+NUM_ROOMS);
-	/* exit if file(s) couldn't be created */
-	if(file_status == 1)
-		exit(1);
-
-	/* read start file and present to user */
-	result = doRoomActions(dirName, rooms[startPt], &input, &path);
-
-	/* continue through the remaining rooms */
-	while(result != 1){
-		/* read file selected by user and present */
-		result = doRoomActions(dirName, input, &input, &path);
-		/* increment turn counter */
-		roomCount++;
-	}
-
-	/* if you're out of the loop, that means the last room has been found */
-	doEnding(path, roomCount);
-	
-	/* free variables initialized with malloc */
-	free(input);
-	free(path);
-
-	/* end program with no errors */
-	return 0;
-}
-
