74e2432b6d47076e85c4174e1295d5a2103b6f3a
Collin James <mail@collinjam.es>
2016-11-26 20:06:19 -0500

    added comments so far

--- prog4/server.c
+++ prog4/server.c
@@ -21,63 +21,54 @@ int setUpSocket(struct sockaddr_in * serverAddress, int maxConn);
 
 int main(int argc, char *argv[])
 {
-	int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
+	/* Check usage & args */
+	if (argc < 2) { 
+		fprintf(stderr,"SERVER: USAGE: %s port\n", argv[0]); 
+		exit(1); 
+	}
+
+	/* socket variables */
+	int listenSocketFD, 
+		establishedConnectionFD, 
+		portNumber, 
+		charsRead;
+	socklen_t sizeOfClientInfo;
+	struct sockaddr_in serverAddress, clientAddress;
+	
+	/* keeping track of pids and connections */
 	int exitSignal = 0,
 		pid = -1, // for storing the child pid
-		numConnections = 0; // store number of connections (5 max)
-	int status,
+		numConnections = 0, // store number of connections (5 max)
+		status,
 		wpid;
 	struct Pidkeeper thePK = new_PK(pid, exitSignal);
-		
-	socklen_t sizeOfClientInfo;
-	// char buffer[256];
-	struct sockaddr_in serverAddress, clientAddress;
 
-	/* select stuff */
+	/* select variables */
 	fd_set master;    // master file descriptor list
     fd_set read_fds;  // temp file descriptor list for select()
-    int fdmax,
-    	newfdm,
-    	yes = 1; // for setsockopt;
-
+    int fdmax; // for max fd in select
+    
     /* setup select sets by clearing */
     FD_ZERO(&master);
     FD_ZERO(&read_fds);
 
-	if (argc < 2) { fprintf(stderr,"SERVER: USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
-
-	// Set up the address struct for this process (the server)
+	/*Set up the address struct for this process (the server)*/
 	memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
 	portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
 	serverAddress.sin_family = AF_INET; // Create a network-capable socket
 	serverAddress.sin_port = htons(portNumber); // Store the port number
 	serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
 
-	// Set up the socket
+	// Set up the socket and start listen()
 	listenSocketFD = setUpSocket(&serverAddress, maxConnections);
-	// listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
-	// if (listenSocketFD < 0) error("SERVER: ERROR opening socket");
-
-	// // reuse previously used ports before they are released by OS (from beej.us) -- doesn't work
- //    // setsockopt(listenSocketFD, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-
-	// // Enable the socket to begin listening
-	// if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
-	// 	error("SERVER: ERROR on binding");
-	
-	// int listening;
-	// listening = listen(listenSocketFD, maxConnections); // Flip the socket on - it can now receive up to 5 connections
-	// if(listening < 0)
-	// 	error("SERVER: failed to listen");
-
-	// printf("listening=%d\n", listening);
-	// if(listen(listenSocketFD, 5) < 0); // Flip the socket on - it can now receive up to 5 connections
-	// 	error("ERROR on listening");
 
 	/* add listenSocketFD to master set (for select); set the max fd to be the listener */
 	FD_SET(listenSocketFD, &master);
 	fdmax = listenSocketFD;
 
+	/* loop until exitSignal is received (change to endless loop in final code) 
+		In each loop, first collect finished processes, then select an active connection, 
+		then process connections */
 	int printerr = 1;
 	while(exitSignal == 0 && pid != 0) { // do until message is "exit"
 		/* collect finished processes */
@@ -91,6 +82,7 @@ int main(int argc, char *argv[])
 
 		if (printerr)
 			printf("number of connections = %d\n", numConnections);
+
 		/* do your select and make sure there are no errors */
 		if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
             error("SERVER: select failed");
@@ -98,80 +90,71 @@ int main(int argc, char *argv[])
         }
         else { // no errors, look for new connections or data
         	
-        	printf("looking for connections\n");
+        	// printf("looking for connections\n");
         	int i;
         	for (i = 0; i <= fdmax; i++)
         	{
         		if(FD_ISSET(i, &read_fds)) { // we have a connection/data to read
-        			printf("new connection\n");
-        			if(i == listenSocketFD){ // there is a new listener
-        				/* check that you still have available connections and perform handshake */
+        			// printf("new connection\n");
+        			/* there is a new listener */
+        			if(i == listenSocketFD){
         				sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+
+        				/* check that you still have available connections and perform handshake */
 						if(numConnections < maxConnections) {
 							// Accept a connection, blocking if one is not available until one connects
-							
 							establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
 							if (establishedConnectionFD < 0) 
 								error("SERVER: ERROR on accept");
-							else { // add the new connection to the set of connections
+							/* if no error, add new connection to your set */
+							else {
 								numConnections += 1;
-								printf("adding new connection to master\n");
+								// printf("adding new connection to master\n");
+								/* add new connection to master set */
 								FD_SET(establishedConnectionFD, &master);
+
+								/* make a new max if necessary */
 								if(establishedConnectionFD > fdmax)
 									fdmax = establishedConnectionFD;
-								/* don't want to accept any more connections */
+
+								/* don't want to accept any more connections if reached max */
 								if(numConnections >= maxConnections){
 									close(listenSocketFD); // close to reject new connections
-									FD_CLR(i, &master); // remove the listening socket from the set
+									FD_CLR(i, &master); // remove the listening socket from the set; re-add after new client data is read
 								}
 							}
 	        			}
-	        			
-	        			else {
-	        				if(printerr > 0){
-		        				printf("server busy with %d connections. Try again later.\n", numConnections);
-								printerr = 0;
-							}
-	        			}
         			}
-        			else { // we have something to read
-	        			printf("getting data\n");
-	        			/**** do fork here; the following should be in the child ****/
-						
+        			/* we have something to read from a client */
+        			else {
+	        			// printf("getting data\n");
+	        			/* fork a process with doEncrypt... encrypt here */
 						thePK = doEncryptInChild(i);
 						pid = thePK.pid;
 						exitSignal = thePK.status;
-						FD_CLR(i, &master); // remove the connection from the master set
-						numConnections -= 1; // make another connection available
-						/* check for need to restart socket and error printing */
-						if(numConnections < maxConnections){
-							printerr = 1;
-							/* check whether socket needs to be reopened */
+
+						/* remove the connection from the master set and decrement connections */
+						FD_CLR(i, &master);
+						numConnections -= 1;
+
+						/* check for need to restart socket (don't do in child [pid==0]) */
+						if(numConnections < maxConnections && pid != 0){
+							/* check whether socket needs to be reopened (don't do in child [pid==0]) */
 							if(!FD_ISSET(listenSocketFD, &master) && pid != 0) {
 								printf("SERVER: resetting the connection\n");
 								/* re-open the socket */
 								listenSocketFD = setUpSocket(&serverAddress, maxConnections);
-		      //   				int listening;
-								// listening = listen(listenSocketFD, maxConnections); // Flip the socket on - it can now receive up to 5 connections
-								// if(listening < 0)
-								// 	error("SERVER: failed to listen");
-
-								/* add listenSocketFD to master set (for select); set the max fd to be the listener */
+		        				/* add it back to the set and make a new max if necessary */
 								FD_SET(listenSocketFD, &master);
 								if(listenSocketFD > fdmax)
 									fdmax = listenSocketFD;
 							}
 						}
 	        		}
-        		}	
-			}
-			// }
-			// else if(printerr == 1) {
-			// 	printf("server busy with %d connections. Try again later.\n", numConnections);
-			// 	printerr = 0;
-			// }
-		}
-	}
+        		} // END FD_ISSET if	
+			} // END for loop
+		} // END select else
+	} // END main while loop
 
 /**** this is done in the parent at the end of the program ****/
 	if(pid != 0) {
@@ -179,7 +162,7 @@ int main(int argc, char *argv[])
 		/* collect finished processes */
 		do {
 			wpid = waitpid(-1, &status, WNOHANG);
-			printf("wpid=%d\n", wpid);
+			// printf("wpid=%d\n", wpid);
 		} while (wpid > 0);
 	}
 
@@ -190,7 +173,8 @@ int setUpSocket(struct sockaddr_in * serverAddress, int maxConn){
 	// Set up the socket
 	int yes = 1;
 	int listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
-	if (listenSocketFD < 0) error("SERVER: ERROR opening socket");
+	if (listenSocketFD < 0) 
+		error("SERVER: ERROR opening socket");
 
 	// reuse previously used ports before they are released by OS (from beej.us) -- doesn't work
     setsockopt(listenSocketFD, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
@@ -235,7 +219,8 @@ struct Pidkeeper doEncryptInChild(int cnctFD) {
 		int charsRead;
 		memset(buffer, '\0', 256);
 		charsRead = recv(cnctFD, buffer, 255, 0); // Read the client's message from the socket
-		if (charsRead < 0) error("SERVER: ERROR reading from socket");
+		if (charsRead < 0) 
+			error("SERVER: ERROR reading from socket");
 		printf("SERVER: I received this from the client: \"%s\"\n", buffer);
 		if(strcmp(buffer, "exit") == 0)
 			exitSignal = 1;
